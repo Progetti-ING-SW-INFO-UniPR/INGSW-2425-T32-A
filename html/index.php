5ff4551604b3073c8b36de52ac49f3d689344db4
JeremieKorangi <jeremiefwapa.korangi@studenti.unipr.it>
2024-12-16 13:24:47 +0100

    include dell'head e del footer

--- html/index.php
+++ html/index.php
@@ -1,23 +1,8 @@
+
 <!DOCTYPE html>
 <html class="no-js" lang="zxx">
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>EventGrids - Conference and Event HTML Template.</title>
-    <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.svg" />
-
-    <!-- ========================= CSS here ========================= -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="assets/css/LineIcons.3.0.css" />
-    <link rel="stylesheet" href="assets/css/animate.css" />
-    <link rel="stylesheet" href="assets/css/tiny-slider.css" />
-    <link rel="stylesheet" href="assets/css/glightbox.min.css" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-
-</head>
+<?php include "inc/head.php"; ?>
 
 <body>
     <!--[if lte IE 9]>
@@ -412,108 +397,7 @@
     </section>
     <!-- End Call Action Area -->
 
-    <!-- Start Footer Area -->
-    <footer class="footer">
-        <!-- Start Footer Top -->
-        <div class="footer-top">
-            <div class="container">
-                <div class="inner-content">
-                    <div class="row">
-                        <div class="col-lg-4 col-md-6 col-12">
-                            <!-- Single Widget -->
-                            <div class="single-footer f-about">
-                                <div class="logo">
-                                    <a href="index.html">
-                                        <img src="assets/images/logo/dark-logo.svg" alt="#">
-                                    </a>
-                                </div>
-                                <p>A business conference organize by EventGrids In. World’s most influential media,
-                                    entertainment & technology.</p>
-                                <span class="social-title">
-                                    Follow Us On:
-                                </span>
-                                <ul class="social">
-                                    <li>
-                                        <a href="javascript:void(0)"><i class="lni lni-facebook-filled"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0)"><i class="lni lni-twitter-filled"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0)"><i class="lni lni-instagram-filled"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0)"><i class="lni lni-linkedin-original"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <!-- End Single Widget -->
-                        </div>
-                        <div class="col-lg-2 col-md-6 col-12">
-                            <!-- Single Widget -->
-                            <div class="single-footer f-link">
-                                <h3>Quick Links</h3>
-                                <ul>
-                                    <li><a href="javascript:void(0)">Get Direction</a></li>
-                                    <li><a href="javascript:void(0)">Sponsor</a></li>
-                                    <li><a href="javascript:void(0)">What We Offer</a></li>
-                                    <li><a href="javascript:void(0)">Ricent Projects</a></li>
-                                    <li><a href="javascript:void(0)">Insights</a></li>
-                                </ul>
-                            </div>
-                            <!-- End Single Widget -->
-                        </div>
-                        <div class="col-lg-2 col-md-6 col-12">
-                            <!-- Single Widget -->
-                            <div class="single-footer f-link">
-                                <h3>Know More</h3>
-                                <ul>
-                                    <li><a href="javascript:void(0)">About Us</a></li>
-                                    <li><a href="javascript:void(0)">Our Pricing</a></li>
-                                    <li><a href="javascript:void(0)">Faq</a></li>
-                                    <li><a href="javascript:void(0)">Guides</a></li>
-                                    <li><a href="javascript:void(0)">Contact Us</a></li>
-                                </ul>
-                            </div>
-                            <!-- End Single Widget -->
-                        </div>
-                        <div class="col-lg-4 col-md-6 col-12">
-                            <!-- Single Widget -->
-                            <div class="single-footer newsletter">
-                                <h3>Subscribe to our newsletter</h3>
-                                <form action="#" method="get" target="_blank" class="newsletter-form">
-                                    <input name="name" placeholder="Your Name*" required="required" type="text">
-                                    <input name="email" placeholder="Email address*" required="required" type="email">
-                                    <div class="button">
-                                        <button class="btn">Subscribe
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                            <!-- End Single Widget -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--/ End Footer Top -->
-        <!-- Start Copyright Area -->
-        <div class="copyright">
-            <div class="container">
-                <div class="inner-content">
-                    <div class="row">
-                        <div class="col-12">
-                            <p class="copyright-text">Designed and Developed by <a href="https://graygrids.com/"
-                                    rel="nofollow" target="_blank">GrayGrids</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Copyright Area -->
-    </footer>
-    <!--/ End Footer Area -->
+   <?php include "inc/footer.php"; ?>
 
     <!-- ========================= scroll-top ========================= -->
     <a href="#" class="scroll-top">
