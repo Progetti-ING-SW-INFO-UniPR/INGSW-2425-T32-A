9b980213d07bf5a5c495d2532a725708af2b3736
JeremieKorangi <jeremiefwapa.korangi@studenti.unipr.it>
2025-01-16 21:48:01 +0100

    Modifica della home page e scrittura della pagina INDICE del documento word

--- html/index.php
+++ html/index.php
@@ -51,14 +51,14 @@
                                         <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse"
                                             data-bs-target="#submenu-1-1" aria-controls="navbarSupportedContent"
                                             aria-expanded="false" aria-label="Toggle navigation">Pages</a>
-                                        <ul class="sub-menu collapse" id="submenu-1-1">
+                                      <!-- <ul class="sub-menu collapse" id="submenu-1-1">
                                             <li class="nav-item"><a href="about-us.html">About Us</a></li>
                                             <li class="nav-item"><a href="gallery.html">Gallery</a></li>
                                             <li class="nav-item"><a href="pricing.html">Pricing</a></li>
                                             <li class="nav-item"><a href="sponsors.html">Sponsors</a></li>
                                             <li class="nav-item"><a href="mail-success.html">Mail Success</a></li>
                                             <li class="nav-item"><a href="404.html">404 Error</a></li>
-                                        </ul>
+                                        </ul> -->
                                     </li>
                                     <li class="nav-item">
                                         <a href="schedule.html" aria-label="Toggle navigation">Schedule</a>
@@ -82,7 +82,7 @@
                                 </ul>
                             </div> <!-- navbar collapse -->
                             <div class="button">
-                                <a href="pricing.html" class="btn">Get Tickets<i class="lni lni-ticket"></i></a>
+                                <a href="pricing.html" class="btn">Cercare ...<i class="lni lni-ticket"></i></a>
                             </div>
                         </nav>
                         <!-- End Navbar -->
@@ -103,14 +103,10 @@
             <div class="row align-items-center">
                 <div class="col-lg-6 offset-lg-3 col-md-12 col-12">
                     <div class="hero-content">
-                        <h5 class="wow zoomIn" data-wow-delay=".2s"><i class="lni lni-map-marker"></i> Waterfront Hotel,
-                            London</h5>
-                        <h2 class="wow fadeInUp" data-wow-delay=".4s">The largest conference in europe 2023</h2>
-                        <p class="wow fadeInUp" data-wow-delay=".6s">Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. deleniti voluptatum! Natus
-                            beatae laborum veniam distinctio.</p>
+                        <h2 class="wow fadeInUp" data-wow-delay=".4s">EventApp vi propone una serie di evento che corrisponde alle vostre preferenze</h2>
+                     
                         <div class="button wow fadeInUp" data-wow-delay=".8s">
-                            <a href="pricing.html" class="btn ">Buy Ticket Now</a>
+                            <a href="pricing.html" class="btn ">Hashtag @</a>
                         </div>
                     </div>
                 </div>
@@ -131,7 +127,7 @@
                                 <h2 id="daystxt">Days</h2>
                             </div>
                             <div class="box">
-                                <h1 id="hours">00</h1>
+                                <h1 id="hours">24</h1>
                                 <h2 id="hourstxt">Hours</h2>
                             </div>
                             <div class="box">
@@ -156,104 +152,35 @@
             <div class="row">
                 <div class="col-12">
                     <div class="section-title">
-                        <h3 class="wow zoomIn" data-wow-delay=".2s">Why join eventGrids?</h3>
-                        <h2 class="wow fadeInUp" data-wow-delay=".4s">Why you should Join Event</h2>
-                        <p class="wow fadeInUp" data-wow-delay=".6s">There are many variations of passages of Lorem
-                            Ipsum available, but the majority have suffered alteration in some form.</p>
+                        <h3 class="wow zoomIn" data-wow-delay=".2s">Nuovi eventi</h3>
+                      
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-4 col-md-6 col-12 wow fadeInUp" data-wow-delay=".2s">
-                    <!-- Start Single Feature -->
-                    <div class="single-featuer">
-                        <img class="shape" src="assets/images/features/shape.svg" alt="#">
-                        <img class="shape2" src="assets/images/features/shape2.svg" alt="#">
-                        <span class="serial">01</span>
-                        <div class="service-icon">
-                            <i class="lni lni-microphone"></i>
-                        </div>
-                        <h3>Great Speakers</h3>
-                        <p>How you transform your business as tech, consumer, habits industry dynamic change, Find out
-                            from.</p>
-                    </div>
-                    <!-- End Single Feature -->
-                </div>
                 <div class="col-lg-4 col-md-6 col-12 wow fadeInUp" data-wow-delay=".4s">
                     <!-- Start Single Feature -->
                     <div class="single-featuer">
+                    <div class="action-box">
+									<img style="border-radius: 15px;" src="img/event1.jpg" alt="">
+								</div> <br>
                         <img class="shape" src="assets/images/features/shape.svg" alt="#">
                         <img class="shape2" src="assets/images/features/shape2.svg" alt="#">
                         <span class="serial">02</span>
-                        <div class="service-icon">
-                            <i class="lni lni-users"></i>
-                        </div>
-                        <h3>New People</h3>
+                       
+                        <h3>Moto express </h3> <div class="btn btn-success">18/01/2025</div> <br> <br>
                         <p>How you transform your business as tech, consumer, habits industry dynamic change, Find out
-                            from.</p>
+                            from.</p> <br>
+
+                            <div class="service-icon" >
+                            <i class="lni lni-bullhorn"></i> 
+                        </div>
+                        <div style="float:right" class="btn btn-primary">Partecipare</div>
                     </div>
                     <!-- End Single Feature -->
                 </div>
-                <div class="col-lg-4 col-md-6 col-12 wow fadeInUp" data-wow-delay=".6s">
-                    <!-- Start Single Feature -->
-                    <div class="single-featuer">
-                        <img class="shape" src="assets/images/features/shape.svg" alt="#">
-                        <img class="shape2" src="assets/images/features/shape2.svg" alt="#">
-                        <span class="serial">03</span>
-                        <div class="service-icon">
-                            <i class="lni lni-bullhorn"></i>
-                        </div>
-                        <h3>Global Event</h3>
-                        <p>How you transform your business as tech, consumer, habits industry dynamic change, Find out
-                            from.</p>
-                    </div>
-                    <!-- End Single Feature -->
-                </div>
-                <div class="col-lg-4 col-md-6 col-12 wow fadeInUp" data-wow-delay=".2s">
-                    <!-- Start Single Feature -->
-                    <div class="single-featuer">
-                        <img class="shape" src="assets/images/features/shape.svg" alt="#">
-                        <img class="shape2" src="assets/images/features/shape2.svg" alt="#">
-                        <span class="serial">04</span>
-                        <div class="service-icon">
-                            <i class="lni lni-heart"></i>
-                        </div>
-                        <h3>Get Inspired</h3>
-                        <p>How you transform your business as tech, consumer, habits industry dynamic change, Find out
-                            from.</p>
-                    </div>
-                    <!-- End Single Feature -->
-                </div>
-                <div class="col-lg-4 col-md-6 col-12 wow fadeInUp" data-wow-delay=".4s">
-                    <!-- Start Single Feature -->
-                    <div class="single-featuer">
-                        <img class="shape" src="assets/images/features/shape.svg" alt="#">
-                        <img class="shape2" src="assets/images/features/shape2.svg" alt="#">
-                        <span class="serial">05</span>
-                        <div class="service-icon">
-                            <i class="lni lni-cup"></i>
-                        </div>
-                        <h3>Networking Session</h3>
-                        <p>How you transform your business as tech, consumer, habits industry dynamic change, Find out
-                            from.</p>
-                    </div>
-                    <!-- End Single Feature -->
-                </div>
-                <div class="col-lg-4 col-md-6 col-12 wow fadeInUp" data-wow-delay=".6s">
-                    <!-- Start Single Feature -->
-                    <div class="single-featuer">
-                        <img class="shape" src="assets/images/features/shape.svg" alt="#">
-                        <img class="shape2" src="assets/images/features/shape2.svg" alt="#">
-                        <span class="serial">06</span>
-                        <div class="service-icon">
-                            <i class="lni lni-happy"></i>
-                        </div>
-                        <h3>Meet New Faces</h3>
-                        <p>How you transform your business as tech, consumer, habits industry dynamic change, Find out
-                            from.</p>
-                    </div>
-                    <!-- End Single Feature -->
-                </div>
+             
+           
             </div>
         </div>
     </section>
